8ad780e863f7dc44ff5208d2c3ee78599092dce0
Karthik Chandrasekar <karthidec1@gmail.com>
2014-04-12 17:00:07 -0700

    One iteration done

--- KMeans.m
+++ KMeans.m
@@ -3,31 +3,61 @@ delimiter = '';
 M = dlmread(filename, delimiter);
 [Mrow, Mcol] = size(M);
 clusters = 3;
+centroidMatrix = zeros(clusters, Mcol);
 
-centroidIndex = zeros(clusters, 1);
+%Initially picking seeds randomly
 for k=1:clusters
-    centroidIndex(k) = randi([1, Mrow], 1,1);
+    centroidMatrix(k,:) = M(randi([1, Mrow], 1,1), :);
 end
+disp(centroidMatrix);
 
-inputCentroidMap = zeros(Mrow,1);
+%The following procedure has to be repeated for r times
 
+%CentroidMap which maps the instance to a cluster
+centroidMap = zeros(Mrow,1);
+
+%Updating centroid
+ centroidInstanceCount = zeros(clusters, 1);
+ newCentroid = zeros(clusters, Mcol);
+
+%Single iteration to cluster instances
 for index = 1:Mrow
     inputVect = M(index, :);
     minVal = 1000000;
     mindIndex = 0;
     for kVal = 1:clusters
-        disp(kVal);
-        centroid = M(centroidIndex(kVal), :); 
-        result = inputVect - centroid;
-        result = result.*result;
-        dist = sum(result);
+        centroid = centroidMatrix(kVal, :); 
+        
+        %Gives euclidean distance between two vectors
+        dist = norm(centroid - inputVect);
+        
         if dist == min([dist minVal])
-            minIndex = centroidIndex(k);
+            minVal = dist;
+            minIndex = kVal;
         end
     end
-    inputCentroidMap(index) = minIndex;    
+    centroidMap(index) = minIndex;
+    
+    %Concurrently updating next centroid support values
+    centroidInstanceCount(minIndex) = centroidInstanceCount(minIndex)+1;
+    newCentroid(minIndex, :) = newCentroid(minIndex, :) + M(index, :);
 end
-%disp(inputCentroidMap);
-disp(centroidIndex);
 
+%Compute new centroid
+for index = 1:clusters
+    newCentroid(index, :) = newCentroid(index, :)/centroidInstanceCount(index); 
+end
+disp('Old centroid  - ');
+disp(centroidMatrix);
+
+disp('New centroid - ');
+disp(newCentroid);
+
+diffCentroid = abs(centroidMatrix - newCentroid);
+disp('Diff centroid');
+disp(diffCentroid);
+
+disp('Max difference ');
+maxDiff = max(max(diffCentroid));
+disp(maxDiff);
 
