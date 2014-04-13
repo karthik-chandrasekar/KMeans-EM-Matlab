d0dd750abb75d0e7c7d8866a5aea1451db99b218
Karthik Chandrasekar <karthidec1@gmail.com>
2014-04-12 17:09:06 -0700

    Single full run

--- KMeans.m
+++ KMeans.m
@@ -13,51 +13,62 @@ disp(centroidMatrix);
 
 %The following procedure has to be repeated for r times
 
-%CentroidMap which maps the instance to a cluster
-centroidMap = zeros(Mrow,1);
+convergenceCount = 0;
+maxDiff = 1
 
-%Updating centroid
- centroidInstanceCount = zeros(clusters, 1);
- newCentroid = zeros(clusters, Mcol);
-
-%Single iteration to cluster instances
-for index = 1:Mrow
-    inputVect = M(index, :);
-    minVal = 1000000;
-    mindIndex = 0;
-    for kVal = 1:clusters
-        centroid = centroidMatrix(kVal, :); 
-        
-        %Gives euclidean distance between two vectors
-        dist = norm(centroid - inputVect);
-        
-        if dist == min([dist minVal])
-            minVal = dist;
-            minIndex = kVal;
-        end
-    end
-    centroidMap(index) = minIndex;
+while maxDiff > 0.001
     
-    %Concurrently updating next centroid support values
-    centroidInstanceCount(minIndex) = centroidInstanceCount(minIndex)+1;
-    newCentroid(minIndex, :) = newCentroid(minIndex, :) + M(index, :);
-end
+    convergenceCount = convergenceCount +1;
+    %CentroidMap which maps the instance to a cluster
+    centroidMap = zeros(Mrow,1);
 
-%Compute new centroid
-for index = 1:clusters
-    newCentroid(index, :) = newCentroid(index, :)/centroidInstanceCount(index); 
-end
-disp('Old centroid  - ');
-disp(centroidMatrix);
+    %Updating centroid
+    centroidInstanceCount = zeros(clusters, 1);
+    newCentroid = zeros(clusters, Mcol);
 
-disp('New centroid - ');
-disp(newCentroid);
+    %Single iteration to cluster instances
+    for index = 1:Mrow
+        inputVect = M(index, :);
+        minVal = 1000000;
+        mindIndex = 0;
+        for kVal = 1:clusters
+            centroid = centroidMatrix(kVal, :); 
+        
+             %Gives euclidean distance between two vectors
+            dist = norm(centroid - inputVect);
+        
+            if dist == min([dist minVal])
+                minVal = dist;
+                minIndex = kVal;
+            end
+        end
+        centroidMap(index) = minIndex;
+    
+        %Concurrently updating next centroid support values
+        centroidInstanceCount(minIndex) = centroidInstanceCount(minIndex)+1;
+        newCentroid(minIndex, :) = newCentroid(minIndex, :) + M(index, :);
+    end
 
-diffCentroid = abs(centroidMatrix - newCentroid);
-disp('Diff centroid');
-disp(diffCentroid);
+    %Compute new centroid
+    for index = 1:clusters
+        newCentroid(index, :) = newCentroid(index, :)/centroidInstanceCount(index); 
+    end
+    disp('Old centroid  - ');
+    disp(centroidMatrix);
 
-disp('Max difference ');
-maxDiff = max(max(diffCentroid));
-disp(maxDiff);
+    disp('New centroid - ');
+    disp(newCentroid);
+
+    diffCentroid = abs(centroidMatrix - newCentroid);
+    disp('Diff centroid');
+    disp(diffCentroid);
+
+    disp('Max difference ');
+    maxDiff = max(max(diffCentroid));
+    disp(maxDiff);
+    
+    %copy new centroid to the old centroid
+    centroidMatrix = newCentroid;
+    disp(convergenceCount);
+end    
 
